9fd7c8d04772a3bbe6107bb9437bcc66cf342705
divine-chidi <divinechidi36@gmail.com>
2023-02-16 16:05:45 -0800

    101-quote.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,7 @@
  */
 int main(void)
 {
+
 	printf("Size of a char: %i byte(s)\n",	sizeof(char));
 	printf("Sise of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
