066594c389951a5dd846d594393745103916f580
divine-chidi <divinechidi36@gmail.com>
2023-02-17 16:54:12 -0800

    6-print_numbers.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	int i;
+	int a;
 
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-	printf("\n");
-
+	for (a = 48; a < 58; a++)
+		putchar(a);
+	putchar('\n');
 	return (0);
 }
