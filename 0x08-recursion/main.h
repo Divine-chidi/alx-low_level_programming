b430a49b48b6b50878ec27b8755a0a9bb2231525
divine-chidi <divinechidi36@gmail.com>
2023-03-08 08:52:12 -0800

    code that prints a sting

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+int _putchar(char c);
+void _puts_recusion(char *s);
+void _prints_rev_recursion(chac *s)
+int _strlen_recusion(char *s);
+int factorial(int n);
+int _pow_recusion(int x, int y);
+int _squrt_recusion(int n);
+int is_prime_number(int n);
+int is_palimdrome(char *s);
+int wildcmp(char *s1, char *s2);
